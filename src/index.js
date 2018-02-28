0c833b25decd2406391794f85630168d34c5220f
Haruna <haruna0510@kif.biglobe.ne.jp>
2018-02-28 22:14:23 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by haruna on 2/26/18.
+ */
+import sum from './sum'
+
+const total = sum(10, 5)
+
+console.log(total)
---
+++ src/sum.js
@@ -0,0 +1,5 @@
+/**
+ * Created by haruna on 2/26/18.
+ */
+const sum = (a, b) => a + b;
+export default sum;
